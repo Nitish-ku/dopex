782d9bb100f0be4eac0e7656a0ffa161975bf925
Nitish Kumar <nitish.ksharmaa@gmail.com>
2025-07-24 13:10:03 +0530

    day 6 updated

--- frontend/src/pages/Layout.jsx
+++ frontend/src/pages/Layout.jsx
@@ -3,7 +3,7 @@ import { Outlet, useNavigate } from 'react-router-dom'
 import { assets } from '../assets/assets'
 import { Menu, X } from 'lucide-react' 
 import Sidebar from '../components/Sidebar'
-import { useUser } from '@clerk/clerk-react'
+import { SignIn, useUser } from '@clerk/clerk-react'
 
 const Layout = () => {
   const navigate = useNavigate()
@@ -13,7 +13,7 @@ const Layout = () => {
     <div className='flex flex-col items-start justify-start h-screen'>
       <nav className='w-full px-8 min-h-14 flex items-center justify-between 
       border-b border-gray-200'>
-        <img src={assets.logo} alt='logo' onClick={()=>navigate('/')} className='w-28' />
+        <img className='cursor-pointer w-32 sm:w-44' src={assets.logo} alt='logo' onClick={()=>navigate('/')} />
         {
           sidebar ? <X onClick={()=> setSidebar(false)} className='w-6 h-6 text-gray-600 sm:hidden'/>
           : <Menu onClick={()=> setSidebar(true)} className='w-6 h-6 text-gray-600 sm:hidden'/>
@@ -29,8 +29,8 @@ const Layout = () => {
         
     </div>
   ) : (
-    <div>
-
+    <div className='flex items-center justify-center h-screen'>
+      <SignIn />
     </div>
   )
 }
