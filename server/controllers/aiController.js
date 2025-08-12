8914c5bfa385ef70842202e063b8a4a1292e4be4
Nitish Kumar <nitish.ksharmaa@gmail.com>
2025-08-12 19:30:31 +0530

    added premium subscription for article generating

--- server/controllers/aiController.js
+++ server/controllers/aiController.js
@@ -13,58 +13,45 @@ const AI = new OpenAI({
 });
 
 
-export const generateArticle = async ( req, res )=>{
-
+export const generateArticle = async (req, res) => {
     try {
         const { userId } = req.auth();
         const { prompt, length } = req.body;
         const plan = req.plan;
-        const free_usage = req.free_usage;
 
-        if(plan !== 'premium' && free_usage >= 10){
-            return res.json({ success: false, message: "Limit reached. Upgrade to continue" })
+        if (plan !== 'premium') {
+            return res.json({ 
+                success: false, 
+                message: "This feature is only available for premium subscriptions" 
+            });
         }
 
-
         const response = await AI.chat.completions.create({
-        model: "gemini-2.0-flash",
-        messages: [
-            {
-                role: "user",
-                content: prompt,
-            },
-
-        ],
-        temperature: 0.7,
-        max_tokens: length,
+            model: "gemini-2.0-flash",
+            messages: [
+                {
+                    role: "user",
+                    content: prompt,
+                },
+            ],
+            temperature: 0.7,
+            max_tokens: length,
         });
 
         const content = response.choices[0].message.content;
         
-        await sql` INSERT INTO creations (user_id, prompt, content, type) VALUES (${userId}, ${prompt}, ${content}, 'article')`;
+        await sql`
+            INSERT INTO creations (user_id, prompt, content, type) 
+            VALUES (${userId}, ${prompt}, ${content}, 'article')
+        `;
 
-        if (plan !== 'premium') {
-            await clerkClient.users.updateUserMetadata(userId, {
-                privateMetadata:{
-                    free_usage: free_usage + 1
-                }
-            })
-            
-        }
-
-        res.json({success: true, content})
-
-
-
+        res.json({ success: true, content });
 
     } catch (error) {
         console.log(error.message);
-        res.json({success: false, message: error.message})
-        
+        res.json({ success: false, message: error.message });
     }
-
-}
-
+};
 
 
 
