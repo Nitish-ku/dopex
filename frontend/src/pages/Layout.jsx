3e4b9f7f9bd995157a3e4b58ba38b84e4f2cdddb
Nitish Kumar <nitish.ksharmaa@gmail.com>
2025-08-12 20:33:08 +0530

    the ai page is done as of now

--- frontend/src/pages/Layout.jsx
+++ frontend/src/pages/Layout.jsx
@@ -12,16 +12,16 @@ const Layout = () => {
   return user ? (
     <div className='flex flex-col items-start justify-start h-screen'>
       <nav className='w-full px-8 min-h-14 flex items-center justify-between 
-      border-b border-gray-200 bg-white'>
+      border-b border-gray-800 bg-[#0b0f14]'>
         <img className='cursor-pointer w-32 sm:w-44' src={assets.logo} alt='logo' onClick={()=>navigate('/')} />
         {
-          sidebar ? <X onClick={()=> setSidebar(false)} className='w-6 h-6 text-gray-600 sm:hidden'/>
-          : <Menu onClick={()=> setSidebar(true)} className='w-6 h-6 text-gray-600 sm:hidden'/>
+          sidebar ? <X onClick={()=> setSidebar(false)} className='w-6 h-6 text-gray-300 sm:hidden'/>
+          : <Menu onClick={()=> setSidebar(true)} className='w-6 h-6 text-gray-300 sm:hidden'/>
         }
       </nav>
       <div className='flex-1 w-full flex h-[calc(100vh-64px)]'>
         <Sidebar sidebar={sidebar} setSidebar={setSidebar}/>
-        <div className='flex-1 bg-[#F4F7FB] overflow-y-auto'>
+        <div className='flex-1 bg-[#0b0f14] overflow-y-auto'>
           <Outlet />
         </div>
       </div>
